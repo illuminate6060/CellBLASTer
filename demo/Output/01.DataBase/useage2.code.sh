df63b14a29e95aa6a44c5bf0ec143093d7d5c35b
Lin Du <50705363+illuminate6060@users.noreply.github.com>
2026-09-02 21:44:25 +0800

    Delete demo/Output/01.DataBase directory

--- demo/Output/01.DataBase/useage2.code.sh
+++
@@ -1,9 +0,0 @@
-python CellBlaster.py \
-  -t Dicot \
-  -p Root \
-  -s  SRP309176 \
-  -o ./Output \
-  -q ./Query_SRP285040.h5ad \
-  -qs  SRP250946 \
-  --reference-adata  ./Reference_SRP285040.h5ad \
-  --reference-symbol  SRP285040 
